ef222103ba94220342d2ba9c5e7804ac38ea06ac
Cute_Wisp <sweatpotato13@gmail.com>
2022-08-07 11:38:52 +0900

    feat: 🎸 add discord bot client settings

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import "reflect-metadata";
+import 'dotenv/config';
+
+import { dirname, importx } from "@discordx/importer";
+import type { Interaction, Message } from "discord.js";
+import { IntentsBitField } from "discord.js";
+import { Client } from "discordx";
+
+export const bot = new Client({
+    botGuilds: [client => client.guilds.cache.map(guild => guild.id)],
+
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.GuildMessageReactions,
+        IntentsBitField.Flags.GuildVoiceStates,
+    ],
+
+    silent: false,
+
+    simpleCommand: {
+        prefix: "!",
+    },
+});
+
+bot.once("ready", async () => {
+    await bot.guilds.fetch();
+    await bot.initApplicationCommands();
+    console.log("Bot started");
+});
+
+bot.on("interactionCreate", (interaction: Interaction) => {
+    bot.executeInteraction(interaction);
+});
+
+bot.on("messageCreate", (message: Message) => {
+    bot.executeCommand(message);
+});
+
+async function run() {
+    await importx(dirname(import.meta.url) + "/{events,commands}/**/*.{ts,js}");
+
+    // Let's start the bot
+    if (!process.env.BOT_TOKEN) {
+        throw Error("Could not find BOT_TOKEN in your environment");
+    }
+
+    // Log in with your bot token
+    await bot.login(process.env.BOT_TOKEN);
+}
+
+run();
