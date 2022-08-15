58f81b05067bbd59440b1f6abddfaf507277c20e
CuteWisp <sweatpotato13@gmail.com>
2022-08-15 12:15:43 +0900

    feat: 🎸 add wallet command

--- src/commands/nightmarket.ts
+++ src/commands/nightmarket.ts
@@ -1,17 +1,42 @@
+import axios from "axios";
 import type { CommandInteraction, Message } from "discord.js";
 import {
   Discord,
   Slash,
+  SlashOption,
 } from "discordx";
 
 @Discord()
 export class Nightmarket {
   @Slash("nightmarket", { description: "show your Valorant Night Market." })
-  slashLikeIt(command: CommandInteraction): void {
-    this.nightmarket(command);
+  slashNightmarket(
+    @SlashOption("authorization", { description: "Authorization" })
+    authorization: string,
+    @SlashOption("entitlements", { description: "Entitlements" })
+    entitlements: string,
+    @SlashOption("region", { description: "account region" })
+    region: string,
+    @SlashOption("puuid", { description: "puuid" })
+    puuid: string,
+    interaction: CommandInteraction): void {
+    this.nightmarket(region, puuid, authorization, entitlements, interaction);
   }
 
-  nightmarket(command: CommandInteraction | Message): void {
-    command.reply("I like it, Thanks");
+  async nightmarket(region: string, puuid: string, authorization: string, entitlements: string, interaction: CommandInteraction): Promise<void> {
+    const response = await getNightmarket(region, puuid, authorization, entitlements);
+    const data = response.data;
+    console.log(data);
+    interaction.reply("I like it, Thanks");
   }
+}
+
+async function getNightmarket(region: string, puuid: string, authorization: string, entitlements: string) {
+  return await axios({
+    url: `https://pd.${region}.a.pvp.net/store/v2/storefront/${puuid}`,
+    method: 'GET',
+    headers: {
+      'X-Riot-Entitlements-JWT': entitlements,
+      'Authorization': authorization
+    }
+  });
 }
--- src/commands/wallet.ts
+++ src/commands/wallet.ts
@@ -1,29 +1,57 @@
 import axios from "axios";
-import type { CommandInteraction, Message } from "discord.js";
+import { CommandInteraction, EmbedBuilder, Message } from "discord.js";
 import {
   Discord,
   Slash,
+  SlashOption,
 } from "discordx";
 
 @Discord()
 export class Wallet {
   @Slash("wallet", { description: "show your Valorant Wallet." })
-  slashLikeIt(command: CommandInteraction): void {
-    this.wallet(command);
+  slashWallet(
+    @SlashOption("authorization", { description: "Authorization" })
+    authorization: string,
+    @SlashOption("entitlements", { description: "Entitlements" })
+    entitlements: string,
+    @SlashOption("region", { description: "account region" })
+    region: string,
+    @SlashOption("puuid", { description: "puuid" })
+    puuid: string,
+    interaction: CommandInteraction): void {
+    this.wallet(region, puuid, authorization, entitlements, interaction);
   }
 
-  wallet(command: CommandInteraction | Message): void {
-    getWallet("AP", "");
+  async wallet(region: string, puuid: string, authorization: string, entitlements: string, interaction: CommandInteraction): Promise<void> {
+    try {
+      const response = await getWallet(region, puuid, authorization, entitlements);
+      const data = response.data;
+      console.log(data);
+      const embed = new EmbedBuilder();
+      embed.setColor(0xfa4454)
+      // embed.setThumbnail(data.data.images.large)
+      embed.addFields(
+        { name: `Valorant Points`, value: `${data.Balances["85ad13f7-3d1b-5128-9eb2-7cd8ee0b5741"]}`, inline: false },
+        { name: `Radianite Points`, value: `${data.Balances["e59aa87c-4cbf-517a-5983-6e81511be9b7"]}`, inline: false },
+      )
+      embed.setTimestamp()
+
+      interaction.channel?.send({ embeds: [embed] });
+      interaction.reply(`Balance`);
+    } catch (error: any) {
+      console.log(error.message);
+      interaction.reply(`There was an error while executing this command!, Please try again later`);
+    }
   }
 }
 
-async function getWallet(region: string, puuid: string) {
+async function getWallet(region: string, puuid: string, authorization: string, entitlements: string) {
   return await axios({
     url: `https://pd.${region}.a.pvp.net/store/v1/wallet/${puuid}`,
     method: 'GET',
     headers: {
-      'X-Riot-Entitlements-JWT': 'RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)',
-      'Authorization': 'Authorization'
+      'X-Riot-Entitlements-JWT': entitlements,
+      'Authorization': authorization
     }
   });
 }
